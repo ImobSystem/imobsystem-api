ce6bad537909e54210f211c20f45283bb1471b5b
VictorHGomes <victor.couto@ufrpe.br>
2026-07-04 21:41:50 -0300

    feat: endpoint de listagem de corretores
    Adiciona GET /corretores retornando a lista de corretores.

--- src/main/java/com/system/imob/controllers/CorretorController.java
+++ src/main/java/com/system/imob/controllers/CorretorController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/corretores")
 public class CorretorController {
@@ -18,5 +20,10 @@ public class CorretorController {
     public ResponseEntity<CorretorResponseDTO> cadastrarCorretor (@RequestBody CorretorRequestDTO dto){
         CorretorResponseDTO response = corretorService.cadastrarCorretor(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CorretorResponseDTO>> listarCorretores(){
+        return ResponseEntity.ok(corretorService.listarCorretores());
     }
 }
