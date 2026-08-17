7d9deefc7674cb521a03ff59bcf9c51efa9505c8
VictorHGomes <victor.couto@ufrpe.br>
2026-08-17 14:31:38 -0300

    fix: adicionando tipoCliente no ClienteResponseDTO (tipo de cliente nao aparecia na listagem)

--- src/main/java/com/system/imob/repositories/CorretorRepository.java
+++ src/main/java/com/system/imob/repositories/CorretorRepository.java
@@ -2,8 +2,11 @@ package com.system.imob.repositories;
 
 import com.system.imob.models.Corretor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface CorretorRepository extends JpaRepository<Corretor, Long> {
     Optional<Corretor> findByEmail(String email);
+
+    List<Corretor> findByImobiliariaId(Long imobiliariaId);
 }
